61d2a15d04c35cfedfd9e2f69c288fcb506e5ea7
MumtazBMalik <123621068+MumtazBMalik@users.noreply.github.com>
2025-03-04 16:06:12 +0200

    Update src/app/shared/services/auth.service.ts
    Co-authored-by: Pamela Lotriet <98311593+pamelaLotrietRetro@users.noreply.github.com>

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { authRequest, authResponse } from '@app/shared/models/auth.model';
+import { authRequest, authResponse } from '@models/auth.model';
 
 @Injectable({
   providedIn: 'root',
