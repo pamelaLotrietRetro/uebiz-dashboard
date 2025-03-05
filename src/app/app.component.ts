81e3d1fb498e9a6d141bd1f90773ced5adc88a2d
Beloved-Nethengwe <bnethengwe@retrorabbit.co.za>
2025-03-05 09:04:51 +0200

    test lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ButtonModule } from "primeng/button";
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, ButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'uebiz-dashboard';
+  title = "uebiz-dashboard";
 }
