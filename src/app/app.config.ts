801d12edb0e2db5fcb2cfce190fbbec4cde36986
MumtazBMalik <mmalik@retrorabbit.co.za>
2025-03-05 11:17:30 +0200

    testing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { CustomTranslateLoader } from '@services/translate.loader.service';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
@@ -12,6 +15,16 @@ import { authFeatureKey, authReducer } from '@store/authState/auth.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
@@ -36,3 +49,8 @@ export const appConfig: ApplicationConfig = {
     }),
   ],
 };
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new CustomTranslateLoader(http);
+}
